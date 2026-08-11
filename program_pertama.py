124fd88f764374cb92599494ad1e6791c6528190
abdr2zcq <arq000016@gmail.com>
2026-08-11 16:48:59 +0700

    Add files via upload

---
+++ program_pertama.py
@@ -0,0 +1,11 @@
+# Program Pertamaku
+# Nama: Abdur Rozzaq
+# Kelas: X RPL 1
+
+print("====================================")
+print("     SELAMAT DATANG DI PYTHON!")
+print("====================================")
+print()
+print("Program ini dibuat oleh:", "Abdur Rozzaq")
+print("Sekolah: SMK TJP Tuban")
+print("Tahun  :", 2026)
