62441a711ddbb6c050b9e955b7efb8514b6522a3
abdr2zcq <arq000016@gmail.com>
2026-09-08 03:01:47 -0700

    Add files via upload

---
+++ bab3/contoh_pertama.py
@@ -0,0 +1,13 @@
+# Ini adalah komentar - tidak dijalankan oleh Python
+# Program: Salam Perkenalan 
+
+print("Halo, Dunia!")            # Menampilkan teks
+print("Nama saya: Python")        # Setiap print() = satu baris output
+print("Versi: 3.x")              # Tanda kutip bisa ganda atau tunggal
+
+print("Halo, Dunia")
+print('Belajar Python')
+print(10+5)
+print("Jumlah:", 10+5)
+print()   # kosong
+
